4623cb4582f8d4da3cec655cb5e0763f61a6feb5
DiliniChethana <167061217+DiliniChethana@users.noreply.github.com>
2025-03-04 21:22:46 +0530

    committed

---
+++ list.py
@@ -0,0 +1,5 @@
+names=["dilini","chethi","kasuni","Thilakarathna"]
+print(names)
+names.append("Ruwan")
+names.sort()
+print(names)
---
+++ sequence.py
@@ -0,0 +1,5 @@
+name="chethana"
+print(name[0])
+names=["dilini","chethi","thilakarathna"]
+print(names)
+print(names[1])
